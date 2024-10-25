962c1a7aca5bc62856b793026a5159ebc04063b5
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-10-25 11:31:45 +0200

    ipcbench: do benchmark on all CPUs in parallel
    Change-Id: I9248c298471b1435bf220bd2e9f03b309ca27df3

--- src/ipcbench.c
+++ src/ipcbench.c
@@ -17,6 +17,21 @@
 struct Caller_params
 {
   l4_cap_idx_t responder_cap;
+  unsigned cpu;
+};
+
+struct Pair
+{
+  pthread_t caller_thread;
+  struct Caller_params caller_params;
+  pthread_t responder_thread;
+};
+
+struct Spawn_param
+{
+  unsigned idx;
+  unsigned num_cpus;
+  struct Pair *pairs;
 };
 
 enum { Enable_return_checks = 1 };
@@ -61,6 +76,13 @@ static void check_pthr_err(int r, char const *msg)
       printf("error: %s: %s (%d)\n", msg, strerror(r), r);
       exit(1);
     }
+}
+
+static long run_thread(l4_cap_idx_t thread, unsigned cpu)
+{
+  l4_sched_param_t sp = l4_sched_param(2, 0);
+  sp.affinity = l4_sched_cpu_set(cpu, 0, 1);
+  return l4_error(l4_scheduler_run_thread(l4re_env()->scheduler, thread, &sp));
 }
 
 /* This thread is initiating IPC */
@@ -121,35 +143,83 @@ static void *fn_responder(void *ignore)
   return NULL;
 }
 
+static void spawn_pair(unsigned cpu, void *arg)
+{
+  struct Spawn_param *param = (struct Spawn_param *)arg;
+
+  if (param->idx >= param->num_cpus)
+    {
+      printf("CPU %u showed up late! Ignoring...\n", cpu);
+      return;
+    }
+
+  unsigned idx = param->idx++;
+  struct Pair *pair = &param->pairs[idx];
+
+  // Create threads without starting them. We want to put them directly on the
+  // right CPU...
+  pthread_attr_t attr;
+  check_pthr_err(pthread_attr_init(&attr), "pthread_attr_init");
+  attr.create_flags |= PTHREAD_L4_ATTR_NO_START;
+
+  // Create responder first
+  check_pthr_err(pthread_create(&pair->responder_thread, &attr, fn_responder,
+                                NULL),
+                 "create responder thread");
+
+  long err = run_thread(pthread_l4_cap(pair->responder_thread), cpu);
+  if (err)
+    {
+      printf("Error starting responder on CPU %u: %ld\n", cpu, err);
+      exit(1);
+    }
+
+  // Wait for responder to be ready
+  l4_utcb_t *utcb = l4_utcb();
+  if (l4_ipc_error(l4_ipc_call(pthread_l4_cap(pair->responder_thread), utcb,
+                               l4_msgtag(0, 0, 0, 0), L4_IPC_NEVER),
+                   utcb))
+    printf("Error syncing with responder thread!\n");
+
+  // Now we can create the caller
+  pair->caller_params.responder_cap = pthread_l4_cap(pair->responder_thread);
+  pair->caller_params.cpu = cpu;
+  check_pthr_err(pthread_create(&pair->caller_thread, &attr, fn_caller,
+                                &pair->caller_params),
+                 "create caller thread");
+
+  err = run_thread(pthread_l4_cap(pair->caller_thread), cpu);
+  if (err)
+    {
+      printf("Error starting caller on CPU %u: %ld\n", cpu, err);
+      exit(1);
+    }
+}
+
 int main(int argc, char **argv)
 {
   (void)argc;
   (void)argv;
   unsigned num_cpus = 0;
   enumerate_cpus(&count_cpus, &num_cpus);
-  printf("Found %u CPUs\n", num_cpus);
+  printf("Found %u CPUs. Measuring core-local IPC latency on all CPUs.\n",
+         num_cpus);
 
-  pthread_t thread_caller;
-  pthread_t thread_responder;
+  // Spawn IPC benchmark pairs on all CPUs.
+  struct Pair pairs[num_cpus];
+  struct Spawn_param sp = {0, num_cpus, pairs};
+  enumerate_cpus(&spawn_pair, &sp);
 
-  check_pthr_err(pthread_create(&thread_responder, NULL, fn_responder, NULL),
-                 "create responder thread");
-
-  // Wait for responder to be ready
-  l4_utcb_t *utcb = l4_utcb();
-  if (l4_ipc_error(l4_ipc_call(pthread_l4_cap(thread_responder), utcb,
-                               l4_msgtag(0, 0, 0, 0), L4_IPC_NEVER),
-                   utcb))
-    printf("Error syncing with responder thread!\n");
-
-  struct Caller_params cp;
-  cp.responder_cap = pthread_l4_cap(thread_responder);
-  check_pthr_err(pthread_create(&thread_caller, NULL, fn_caller, &cp),
-                 "create caller thread");
-
-  check_pthr_err(pthread_join(thread_caller, NULL), "join caller thread");
-  check_pthr_err(pthread_cancel(thread_responder), "cancel responder thread");
-  check_pthr_err(pthread_join(thread_responder, NULL), "join responder thread");
+  // Wait for tests to finish
+  for (unsigned i = 0; i < sp.idx; i++)
+    {
+      check_pthr_err(pthread_join(pairs[i].caller_thread, NULL),
+                     "join caller thread");
+      check_pthr_err(pthread_cancel(pairs[i].responder_thread),
+                     "cancel responder thread");
+      check_pthr_err(pthread_join(pairs[i].responder_thread, NULL),
+                     "join responder thread");
+    }
 
   return 0;
 }
